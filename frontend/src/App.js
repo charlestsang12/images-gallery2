5f302dc76619ef74670ae5ff9271656f9a51fc17
Charles <tcccharlesa@gmail.com>
2022-07-24 15:41:08 +0800

    initial header components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,10 +23,13 @@ function App() {
 }
 */
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+     <Header title="Images Gallery"/>
     </div>
   );
 }
