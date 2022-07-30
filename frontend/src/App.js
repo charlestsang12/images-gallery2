cc4bd8527159ceef17ad5b1792195d1144af84dc
Charles <tcccharlesa@gmail.com>
2022-07-30 23:19:02 +0800

    control search components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,16 +22,27 @@ function App() {
   );
 }
 */
-
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
-function App() {
+
+const App = () => {
+  const [word,setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
   return (
-    <div className="App">
+    <div>
      <Header title="Images Gallery"/>
+     <Search word={word} setWord={setWord} handleSumit={handleSearchSubmit}/>
     </div>
   );
 }
 
 export default App;
+
