f5efc9ea1437d54b373dcd23bb4a3918bd2dca18
Charles <tcccharlesa@gmail.com>
2022-08-21 20:23:57 +0800

    API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPALSH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 const App = () => {
   const [word,setWord] = useState('');
@@ -34,7 +35,16 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPALSH_KEY}`)
+    .then((res) =>res.json()) 
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err)=> {
+      console.log(err);
+    })
   }
+
 
   return (
     <div>
